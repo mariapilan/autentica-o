4a5261feecfff20f20d64901febc1fe6a6bae28a
mariapilan <dudapilan06@hotmail.com>
2024-02-15 12:53:48 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Middleware\IsAuthenticated;
+use App\Http\Middleware\SetSanctumGuard;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::post('/create', [AdminController::class, 'store']);
+
+Route::post('/login', [AdminController::class, 'login']);
+
+Route::get('admin/teste', [AdminController::class, 'verificaUsuarioLogado'])
+->middleware([
+        'auth:sanctum',
+        SetSanctumGuard::class,
+        IsAuthenticated::class
+]);
+ 
